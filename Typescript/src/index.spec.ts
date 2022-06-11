cb812ec11efac1b8a188e52383cb3b919cf10ee0
Ben <willcodefortea@gmail.com>
2022-06-11 14:19:57 +0100

    Diagonally taking works

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -1,4 +1,8 @@
-import { canTakeVertically, canTakeHorizontally } from "./index";
+import {
+  canTakeDiagonally,
+  canTakeVertically,
+  canTakeHorizontally,
+} from "./index";
 
 const strToNumber = (s: string) => parseInt(s, 2);
 
@@ -11,5 +15,17 @@ test("two queens cannot be on the same row", () => {
 test("two queens cannot be in the same column", () => {
   expect(
     canTakeVertically(strToNumber("10000000"), strToNumber("10000000"))
+  ).toBe(true);
+});
+
+test("two queens cannot be in the same diagonal l->r", () => {
+  expect(
+    canTakeDiagonally(strToNumber("10000000"), strToNumber("01000000"), 1)
+  ).toBe(true);
+});
+
+test("two queens cannot be in the same diagonal r->l", () => {
+  expect(
+    canTakeDiagonally(strToNumber("01000000"), strToNumber("10000000"), 1)
   ).toBe(true);
 });
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -3,3 +3,14 @@ export const canTakeHorizontally = (queen1: number, queen2: number) =>
 
 export const canTakeVertically = (queen1: number, queen2: number) =>
   (queen1 & queen2) > 0;
+
+export const canTakeDiagonally = (
+  queen1: number,
+  queen2: number,
+  rowDistance: number
+) => {
+  return (
+    canTakeVertically(queen1 >> rowDistance, queen2) ||
+    canTakeVertically(queen1, queen2 >> rowDistance)
+  );
+};
