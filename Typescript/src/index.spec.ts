c367b871cc8c7324fc4734838b79e2b2a62fb8c8
Ben <willcodefortea@gmail.com>
2022-06-11 14:12:28 +0100

    Vertically taking works

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -1,7 +1,15 @@
-import { canTakeHorizontally } from "./index";
+import { canTakeVertically, canTakeHorizontally } from "./index";
 
 const strToNumber = (s: string) => parseInt(s, 2);
 
 test("two queens cannot be on the same row", () => {
-  expect(canTakeHorizontally(strToNumber("10000000"), strToNumber("00100000")));
+  expect(
+    canTakeHorizontally(strToNumber("10000000"), strToNumber("00100000"))
+  ).toBe(true);
+});
+
+test("two queens cannot be in the same column", () => {
+  expect(
+    canTakeVertically(strToNumber("10000000"), strToNumber("10000000"))
+  ).toBe(true);
 });
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,2 +1,5 @@
-export const canTakeHorizontally = (queen1: Number, queen2: Number) =>
+export const canTakeHorizontally = (queen1: number, queen2: number) =>
   queen1 > 0 && queen2 > 0;
+
+export const canTakeVertically = (queen1: number, queen2: number) =>
+  (queen1 & queen2) > 0;
