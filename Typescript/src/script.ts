016b1d2c903c700d1efe9f2243111eebe2157f01
Ben <willcodefortea@gmail.com>
2022-06-11 17:57:42 +0100

    Add bfs to script

--- Typescript/src/script.ts
+++ Typescript/src/script.ts
@@ -1,9 +1,11 @@
 import { printBoard } from ".";
 import { solve as solveDfs } from "./depthFirstSearch";
+import { solve as solveBfs } from "./breadthFirstSearch";
 
 const main = () => {
   let result;
   if (process.argv[2] === "dfs") result = solveDfs();
+  if (process.argv[2] === "bfs") result = solveDfs();
 
   printBoard(result);
 };
